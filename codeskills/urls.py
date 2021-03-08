6ed316e99f957fefafcb658a62555b7aa3de7372
Andres802 <reachandres@gmail.com>
2021-03-08 19:08:01 +0000

    url log in change

--- codeskills/urls.py
+++ codeskills/urls.py
@@ -17,7 +17,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', apis.UserLogin.as_view(), name='login'),
+    path('login/', apis.UserLogin.as_view(), name='login'),
     path('signup/', apis.UserSignup.as_view(), name='signup'),
     path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
